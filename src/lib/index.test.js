04cf85ef159cf64557adc012293be4c5a1a7aa00
Rikin Katyal <me@sirvar.com>
2017-08-31 19:01:27 -0400

    Read only test

--- src/lib/index.test.js
+++ src/lib/index.test.js
@@ -6,16 +6,19 @@ describe('Backend proxy lib', () => {
   let server
   beforeEach(() => {
     server = http.createServer(
-      createHandler({ proxyUrl: 'https://reqres.in/' })
+      createHandler({ proxyUrl: 'https://reqres.in/', readOnly: true })
     )
   })
   it('Proxy JSON properly', async () => {
-    const response = await request(server).get('/posts/1')
+    const response = await request(server).get('/api/users?page=3')
     expect(response.statusCode).toEqual(200)
   })
   it('Get body properly', async () => {
     const response = await request(server).get('/api/users/2')
     expect(response.body.data.id).toEqual(2)
   })
-  
+  it('Test read only', async () => {
+    const response = await request(server).post('/api/users/1')
+    expect(response.statusCode).toEqual(500)
+  })
 })
