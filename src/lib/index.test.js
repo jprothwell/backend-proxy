ea34390fb03e3b3de37afb7d80ef956a2a9f0a92
Rikin Katyal <me@sirvar.com>
2017-08-31 19:10:52 -0400

    Break up tests

--- src/lib/index.test.js
+++ src/lib/index.test.js
@@ -3,22 +3,33 @@ const request = require('supertest')
 const http = require('http')
 
 describe('Backend proxy lib', () => {
-  let server
-  beforeEach(() => {
-    server = http.createServer(
-      createHandler({ proxyUrl: 'https://reqres.in/', readOnly: true })
-    )
+  describe('Test proxy', () => {
+    let server
+    beforeEach(() => {
+      server = http.createServer(
+        createHandler({ proxyUrl: 'https://reqres.in/' })
+      )
+    })
+    it('Proxy JSON properly', async () => {
+      const response = await request(server).get('/api/users?page=3')
+      expect(response.statusCode).toEqual(200)
+    })
+    it('Get body properly', async () => {
+      const response = await request(server).get('/api/users/2')
+      expect(response.body.data.id).toEqual(2)
+    })
   })
-  it('Proxy JSON properly', async () => {
-    const response = await request(server).get('/api/users?page=3')
-    expect(response.statusCode).toEqual(200)
-  })
-  it('Get body properly', async () => {
-    const response = await request(server).get('/api/users/2')
-    expect(response.body.data.id).toEqual(2)
-  })
-  it('Test read only', async () => {
-    const response = await request(server).post('/api/users/1')
-    expect(response.statusCode).toEqual(500)
+  
+  describe('Proxy with read only', () => {
+    let server
+    beforeEach(() => {
+      server = http.createServer(
+        createHandler({ proxyUrl: 'https://reqres.in/', readOnly: true })
+      )
+    })
+    it('Test read only', async () => {
+      const response = await request(server).post('/api/users/1')
+      expect(response.statusCode).toEqual(500)
+    })
   })
 })
