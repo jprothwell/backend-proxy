a3dc2dbc80389aee305ab988dce2173b33b5fd0e
Rikin Katyal <me@sirvar.com>
2017-09-02 15:01:23 -0400

    Existing query

--- src/lib/index.js
+++ src/lib/index.js
@@ -21,6 +21,8 @@ module.exports = function createHandler({ proxyUrl, token, tokenName, readOnly }
 function createUrl(url, req, token, tokenName) {
   if (token == '') {
     return url.replace(/\/$/, '') + req.url
+  } else if (req.url.includes('?')) {
+    return url.replace(/\/$/, '') + req.url + '&' + tokenName + '=' + token
   } else {
     return url.replace(/\/$/, '') + req.url + '?' + tokenName + '=' + token
   }
