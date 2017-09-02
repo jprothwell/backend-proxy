c92028cba252b91a6d4bad021dd75fb0c1f6dd25
Rikin Katyal <me@sirvar.com>
2017-09-02 17:36:57 -0400

    Test

--- src/lib/index.test.js
+++ src/lib/index.test.js
@@ -7,15 +7,15 @@ describe('Backend proxy lib', () => {
     let server
     beforeEach(() => {
       server = http.createServer(
-        createHandler({ proxyUrl: 'https://reqres.in/' })
+        createHandler({ proxyUrl: 'https://reqres.in/api' })
       )
     })
     it('Proxy JSON properly', async () => {
-      const response = await request(server).get('/api/users?page=3')
+      const response = await request(server).get('/users?page=3')
       expect(response.statusCode).toEqual(200)
     })
     it('Get body properly', async () => {
-      const response = await request(server).get('/api/users/2')
+      const response = await request(server).get('/users/2')
       expect(response.body.data.id).toEqual(2)
     })
   })
@@ -24,12 +24,26 @@ describe('Backend proxy lib', () => {
     let server
     beforeEach(() => {
       server = http.createServer(
-        createHandler({ proxyUrl: 'https://reqres.in/', readOnly: true })
+        createHandler({ proxyUrl: 'https://reqres.in/api', readOnly: true })
       )
     })
     it('Test read only', async () => {
-      const response = await request(server).post('/api/users/1')
+      const response = await request(server).post('/users/1')
       expect(response.statusCode).toEqual(500)
+    })
+  })
+
+  describe('Proxy post request', () => {
+    let server
+    beforeEach(() => {
+      server = http.createServer(
+        createHandler({ proxyUrl: 'https://reqres.in/api/' })
+      )
+    })
+    it('Failed post', async () => {
+      const response = await request(server).post('/login', { json: { email: "peter@klaven", password: "cityslicka" } })
+      console.log(response.body)
+      expect(response.body.token).toEqual('QpwL5tke4Pnpja7X')
     })
   })
 })
