1ad3ed50373573f30aad9b8f1b58dc2ae93638cd
Hamza Zia <ziahamza2007@gmail.com>
2017-09-05 00:22:16 +0500

    fixed post requests;

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,18 +4,8 @@ module.exports = function createHandler({ proxyUrl, token, tokenName, readOnly }
   return (req, res) => {
     if (!readOnly || (readOnly && req.method == 'GET')) {
       req.pipe(
-        request({
-          url: createUrl(proxyUrl, req, token, tokenName),
-          method: req.method,
-        }, function(
-          err,
-          response,
-          body
-        ) {
-          res.setHeader('Content-Type', 'application/json')
-          res.end(body.toString())
-        })
-      )
+        request(createUrl(proxyUrl, req, token, tokenName)),
+      ).pipe(res);
     } else {
       inReadOnlyMode(res)
     }
@@ -23,7 +13,7 @@ module.exports = function createHandler({ proxyUrl, token, tokenName, readOnly }
 }
 
 function createUrl(url, req, token, tokenName) {
-  if (token == '') {
+  if (!token) {
     return url.replace(/\/$/, '') + req.url
   } else if (req.url.includes('?')) {
     return url.replace(/\/$/, '') + req.url + '&' + tokenName + '=' + token
